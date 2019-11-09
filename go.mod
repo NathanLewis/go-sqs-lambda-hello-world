f561516f45df8e3e7a25b865f6762fb3f3d584f8
Nathan Lewis <nathanlewis42@gmail.com>
2019-11-09 21:32:08 +0000

    Simple working go sqs poller. Presently for ec2 rather than lambda

--- go.mod
+++ go.mod
@@ -1,14 +1,5 @@
 module github.com/NathanLewis/go-sqs-lambda-hello-world
 
-go 1.12
+go 1.13
 
-require (
-	github.com/alecthomas/kingpin v2.2.6+incompatible // indirect
-	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
-	github.com/alecthomas/units v0.0.0-20190717042225-c3de453c63f4 // indirect
-	github.com/aws/aws-sdk-go v1.23.9
-	github.com/cavaliercoder/grab v2.0.0+incompatible
-	github.com/pkg/errors v0.8.1
-	github.com/sirupsen/logrus v1.4.2
-	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7
-)
+require github.com/aws/aws-sdk-go v1.25.31
--- sqs-poller.go
+++ sqs-poller.go
@@ -1,40 +1,104 @@
+// snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+// snippet-sourceauthor:[Doug-AWS]
+// snippet-sourcedescription:[Enables long polling on message receipt.]
+// snippet-keyword:[Amazon Simple Queue Service]
+// snippet-keyword:[Amazon SQS]
+// snippet-keyword:[GetQueueUrl function]
+// snippet-keyword:[ReceiveMessage function]
+// snippet-keyword:[Go]
+// snippet-sourcesyntax:[go]
+// snippet-service:[sqs]
+// snippet-keyword:[Code Sample]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[2018-03-16]
+/*
+   Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
 package main
 
-// Code taken from https://h2ik.co/2017/01/28/go-sqs-poller/
-
 import (
-	"fmt"
+    "flag"
+    "fmt"
+    "os"
 
-	
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/sqs"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-
-	"github.com/h2ik/go-sqs-poller/worker"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/awserr"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/sqs"
 )
 
-var (
-	aws_access_key string
-	aws_secret_key string
-	aws_region     string
-)
-
+// Receive message from Queue with long polling enabled.
+//
+// Usage:
+//    go run sqs_longpolling_receive_message.go -n queue_name -t timeout
 func main() {
+    var name string
+    var timeout int64
+    flag.StringVar(&name, "n", "", "Queue name")
+    flag.Int64Var(&timeout, "t", 20, "(Optional) Timeout in seconds for long polling")
+    flag.Parse()
 
-	// create a config
-	aws_config := &aws.Config{
-		Credentials: credentials.NewStaticCredentials(aws_access_key, aws_secret_key, ""),
-		Region:      aws.String(aws_region),
-	}
+    if len(name) == 0 {
+        flag.PrintDefaults()
+        exitErrorf("Queue name required")
+    }
 
-	// create the new client with the aws_config passed in
-	svc, url := worker.NewSQSClient("go-webhook-queue-test", aws_config)
-	// set the queue url
-	worker.QueueURL = url
-	// start the worker
-	worker.Start(svc, worker.HandlerFunc(func(msg *sqs.Message) error {
-		fmt.Println(aws.StringValue(msg.Body))
-		return nil
-	}))
+    // Initialize a session in eu-west-1 that the SDK will use to load
+    // credentials from the shared credentials file ~/.aws/credentials.
+    sess, err := session.NewSession(&aws.Config{
+        Region: aws.String("eu-west-1")},
+    )
+
+    // Create a SQS service client.
+    svc := sqs.New(sess)
+
+    // Need to convert the queue name into a URL. Make the GetQueueUrl
+    // API call to retrieve the URL. This is needed for receiving messages
+    // from the queue.
+    resultURL, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
+        QueueName: aws.String(name),
+    })
+    if err != nil {
+        if aerr, ok := err.(awserr.Error); ok && aerr.Code() == sqs.ErrCodeQueueDoesNotExist {
+            exitErrorf("Unable to find queue %q.", name)
+        }
+        exitErrorf("Unable to queue %q, %v.", name, err)
+    }
+
+    for {
+        // Receive a message from the SQS queue with long polling enabled.
+        result, _ := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+            QueueUrl: resultURL.QueueUrl,
+            AttributeNames: aws.StringSlice([]string{
+                "SentTimestamp",
+            }),
+            MaxNumberOfMessages: aws.Int64(10),
+            MessageAttributeNames: aws.StringSlice([]string{
+                "All",
+            }),
+            VisibilityTimeout:   aws.Int64(60),
+            WaitTimeSeconds:     aws.Int64(0),
+        })
+        if len(result.Messages) > 0 {
+            fmt.Printf("Received %d messages.\n", len(result.Messages))
+            fmt.Println(result.Messages)
+        }
+    }
+
 }
 
+func exitErrorf(msg string, args ...interface{}) {
+    fmt.Fprintf(os.Stderr, msg+"\n", args...)
+    os.Exit(1)
+}
